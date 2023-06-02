50cc21625e02940e0e3c11ca0823917fe4bb0a4c
Zhijia Zhu <zhuzhijia@mail.ustc.edu.cn>
2023-06-02 13:25:58 +0800

    Optimize code structure

--- IDEA/src/Collection/PropertiesTest.java
+++ IDEA/src/Collection/PropertiesTest.java
@@ -6,12 +6,13 @@ import java.io.IOException;
 import java.util.Properties;
 
 // 配置文件：工程下的 jdbc.properties
+// TODO -> 20230602 將工程下的jdbc.properties文件移動，优化代码结构！
 public class PropertiesTest {
     public static void main(String[] args) {
         Properties pro = new Properties();
         // 流
         try {
-            FileInputStream fis = new FileInputStream("jdbc.properties");
+            FileInputStream fis = new FileInputStream("Z:\\VSCODE\\Java\\JDBC\\src\\jdbctest.properties");
             pro.load(fis); // 加载流对应的文件
 
             String name = pro.getProperty("name");
